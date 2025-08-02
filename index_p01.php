a98238aa9bb5d462ef1e52b191348c64ffa2e3fa
MizuAsagi0719 <hibawei@gmail.com>
2025-08-02 20:41:39 +0800

    更新剪裁動畫

--- index_p01.php
+++ index_p01.php
@@ -39,9 +39,10 @@
                 </div>
             </div>
         </nav>
+        <div class="demo"></div>
         <div class="container-fluid" id="content">
             <div class="row">
-                <div class="col-md-2">
+                <div class="col-md-2 sidebar">
                     <div class="accordion" id="accordionExample">
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="headingOne">
@@ -122,28 +123,88 @@
                     </div>
                 </div>
                 <div class="col-md-10">
-                    <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
+                    <div id="myCarousel" class="carousel slide" data-bs-ride="true">
                         <div class="carousel-indicators">
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                            <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
                         </div>
                         <div class="carousel-inner">
-                            <div class="carousel-item active">
-                                <img src="./images/0001.jpg" class="d-block w-100" alt="...">
+                            <div class="carousel-item active ">
+                                <svg width="0" height="0" aria-hidden="true">
+                                    <defs>
+                                        <mask id="clip1" maskUnits="userSpaceOnUse" x="0" y="0" width="1600" height="1080">
+                                            <g fill="white">
+                                                <circle class="anim-circle" cx="295.86" cy="353.87" r="220.55" />
+                                                <circle class="anim-circle" cx="564.16" cy="540" r="301.11" />
+                                                <circle class="anim-circle" cx="233.7" cy="746.42" r="217.78" />
+                                                <circle class="anim-circle" cx="564.16" cy="841.11" r="231.43" />
+                                                <circle class="anim-circle" cx="865.26" cy="735.36" r="195.36" />
+                                                <circle class="anim-circle" cx="1297.09" cy="351.28" r="258.39" />
+                                                <circle class="anim-circle" cx="1415.49" cy="766.56" r="128.62" />
+                                                <circle class="anim-circle" cx="1116.84" cy="766.56" r="209.01" />
+                                                <circle class="anim-circle" cx="894.02" cy="434.26" r="248.64" />
+                                                <circle class="anim-circle" cx="669.9" cy="200.24" r="168.9" />
+                                                <path class="anim-path" d="M326.32,0c-6.79,19.26-10.49,39.97-10.49,61.55c0,102.25,82.89,185.15,185.15,185.15s185.14-82.9,185.14-185.15
+                    c0-21.58-3.7-42.29-10.49-61.55H326.32z" />
+                                            </g>
+                                        </mask>
+                                    </defs>
+                                </svg>
+                                <div class="img-wrapper" id="mask1"><img src="./images/0001_1.jpg" class="d-block w-100 "></div>
                             </div>
-                            <div class="carousel-item">
-                                <img src="./images/0002.jpg" class="d-block w-100" alt="...">
+                            <div class="carousel-item ">
+                                <svg width="0" height="0" aria-hidden="true">
+                                    <defs>
+                                        <mask id="clip2" maskUnits="userSpaceOnUse" x="0" y="0" width="1600" height="1080">
+                                            <g fill="white">
+                                                <circle class="anim-circle" cx="584.58" cy="602.12" r="187.54" />
+                                                <circle class="anim-circle" cx="1208.94" cy="735.36" r="301.11" />
+                                                <circle class="anim-circle" cx="263.05" cy="682.9" r="217.78" />
+                                                <circle class="anim-circle" cx="413.95" cy="487.34" r="231.43" />
+                                                <circle class="anim-circle" cx="865.26" cy="735.36" r="195.36" />
+                                                <circle class="anim-circle" cx="1325.84" cy="289.71" r="258.39" />
+                                                <circle class="anim-circle" cx="1440.76" cy="574.42" r="128.62" />
+                                                <circle class="anim-circle" cx="838.83" cy="863" r="209.01" />
+                                                <circle class="anim-circle" cx="984.79" cy="330.33" r="290.03" />
+                                                <circle class="anim-circle" cx="631.07" cy="445.8" r="168.93" />
+                                            </g>
+                                        </mask>
+                                    </defs>
+                                </svg>
+                                <div class="img-wrapper" id="mask2"><img src="./images/0002_1.jpg" class="d-block w-100 "></div>
                             </div>
-                            <div class="carousel-item">
-                                <img src="./images/0003.jpg" class="d-block w-100" alt="...">
+                            <div class="carousel-item ">
+                                <svg width="0" height="0" aria-hidden="true">
+                                    <defs>
+                                        <mask id="clip3" maskUnits="userSpaceOnUse" x="0" y="0" width="1600" height="1080">
+                                            <g fill="white">
+                                                <circle class="anim-circle" cx="946.48" cy="766.56" r="301.11" />
+                                                <circle class="anim-circle" cx="253.41" cy="327.11" r="217.78" />
+                                                <circle class="anim-circle" cx="564.16" cy="841.11" r="231.43" />
+                                                <circle class="anim-circle" cx="137.69" cy="660.61" r="115.71" />
+                                                <circle class="anim-circle" cx="474.5" cy="532.27" r="115.71" />
+                                                <circle class="anim-circle" cx="669.9" cy="597.45" r="115.71" />
+                                                <circle class="anim-circle" cx="795.58" cy="429.17" r="195.36" />
+                                                <circle class="anim-circle" cx="1297.09" cy="351.28" r="258.39" />
+                                                <circle class="anim-circle" cx="1415.49" cy="766.56" r="128.62" />
+                                                <circle class="anim-circle" cx="1116.84" cy="766.56" r="209.01" />
+                                                <circle class="anim-circle" cx="1038.7" cy="248.64" r="248.64" />
+                                                <circle class="anim-circle" cx="669.9" cy="200.24" r="168.93" />
+                                                <path class="anim-path" d="M1600,375.74c-40.39-51.17-102.96-84.01-173.21-84.01c-121.8,0-220.55,98.74-220.55,220.55s98.74,220.55,220.55,220.55
+    		c70.25,0,132.82-32.85,173.21-84.01V375.74z" />
+                                            </g>
+                                        </mask>
+                                    </defs>
+                                </svg>
+                                <div class="img-wrapper" id="mask3"><img src="./images/0003_1.jpg" class="d-block w-100 "></div>
                             </div>
                         </div>
-                        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                        <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Previous</span>
                         </button>
-                        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                        <button class="carousel-control-next" type="button" data-bs-target="#myCarousel" data-bs-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="visually-hidden">Next</span>
                         </button>
@@ -164,10 +225,10 @@
             <div id="carouselExampleFade" class="carousel slide carousel-fade " data-bs-ride="carousel">
                 <div class="carousel-inner">
                     <div class="carousel-item active">
-                        <img src="./images/0004.jpg" class="d-block w-100" alt="...">
+                        <img src="./images/0004.jpg" class="d-block w-100">
                     </div>
                     <div class="carousel-item">
-                        <img src="./images/0005.jpg" class="d-block w-100" alt="...">
+                        <img src="./images/0005.jpg" class="d-block w-100">
                     </div>
                     <div class="row"></div>
                 </div>
@@ -206,34 +267,37 @@
             </div>
         </div>
     </section>
-</body>
-<footer>
-    <div class="container-fluid">
-        <div id="last-data" class="row text-center">
-            <div class="col-sm-3 ">
-                <h4>ABOUT US</h4>
-                <p><a>品牌故事</a></p>
-                <p><a>歷年成果</a></p>
-            </div>
-            <div class="col-sm-3">
-                <h4>顧客服務</h4>
-                <p><a>退換貨政策</a></p>
-                <p><a>隱私政策</a></p>
-                <p><a>運送方式</a></p>
-            </div>
-            <div class="col-sm-3">
-                <h4>門市資訊</h4>
-                <p><a><i class="far fa-phone-alt"></i> tel 04-25229835</a></p>
-                <p><a><i class="fas fa-door-open"></i> 周一至周五 8:00-18:30</a></p>
-                <p><a><i class="fas fa-map-marker-alt"></i>門市地址</a></p>
-            </div>
-            <div class="col-sm-3">
-                <h4>訂閱電子報</h4>
-                <p><input type="email" size="20" placeholder="example@email.com"></p>
+    <footer>
+        <div class="container-fluid">
+            <div id="last-data" class="row text-center">
+                <div class="col-sm-3 ">
+                    <h4>ABOUT US</h4>
+                    <p><a>品牌故事</a></p>
+                    <p><a>歷年成果</a></p>
+                </div>
+                <div class="col-sm-3">
+                    <h4>顧客服務</h4>
+                    <p><a>退換貨政策</a></p>
+                    <p><a>隱私政策</a></p>
+                    <p><a>運送方式</a></p>
+                </div>
+                <div class="col-sm-3">
+                    <h4>門市資訊</h4>
+                    <p><a><i class="far fa-phone-alt"></i> tel 04-25229835</a></p>
+                    <p><a><i class="fas fa-door-open"></i> 周一至周五 8:00-18:30</a></p>
+                    <p><a><i class="fas fa-map-marker-alt"></i>門市地址</a></p>
+                </div>
+                <div class="col-sm-3">
+                    <h4>訂閱電子報</h4>
+                    <p><input type="email" size="20" placeholder="example@email.com"></p>
+                </div>
             </div>
         </div>
-    </div>
-</footer>
+    </footer>
+    <script>
+        
+    </script>
+</body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
     crossorigin="anonymous"></script>
