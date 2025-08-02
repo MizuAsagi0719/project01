1cea086351f8962013f5c5229890447a030babc9
MizuAsagi0719 <hibawei@gmail.com>
2025-08-03 00:49:11 +0800

    建立完成v1.0

--- index_p01.php
+++ index_p01.php
@@ -131,6 +131,11 @@
                         </div>
                         <div class="carousel-inner">
                             <div class="carousel-item active ">
+                                <div class="infoTextR">
+                                    <h2 class="animate__animated animate__backInDown">換季促銷</h2>
+                                    <p class="animate__animated animate__bounceInRight">2件</p>
+                                    <p class="animate__animated animate__bounceIn animate__delay-1s">699</p>
+                                </div>
                                 <svg width="0" height="0" aria-hidden="true">
                                     <defs>
                                         <mask id="clip1" maskUnits="userSpaceOnUse" x="0" y="0" width="1600" height="1080">
@@ -151,9 +156,14 @@
                                         </mask>
                                     </defs>
                                 </svg>
-                                <div class="img-wrapper" id="mask1"><img src="./images/0001_1.jpg" class="d-block w-100 "></div>
+                                <div id="mask1"><img src="./images/0001_1.jpg" class="d-block w-100 "></div>
                             </div>
                             <div class="carousel-item ">
+                                <div class="infoTextL">
+                                    <img src="./images/miyard_logo.png" alt="" class="animate__animated animate__wobble">
+                                    <h2 class="animate__animated animate__fadeInBottomLeft">聯名款<br>甜蜜出擊</h2>
+                                    <p class="animate__animated animate__fadeInUp animate__delay-1s">2025．09<br>comming soon</p>
+                                </div>
                                 <svg width="0" height="0" aria-hidden="true">
                                     <defs>
                                         <mask id="clip2" maskUnits="userSpaceOnUse" x="0" y="0" width="1600" height="1080">
@@ -172,9 +182,14 @@
                                         </mask>
                                     </defs>
                                 </svg>
-                                <div class="img-wrapper" id="mask2"><img src="./images/0002_1.jpg" class="d-block w-100 "></div>
+                                <div id="mask2"><img src="./images/0002_1.jpg" class="d-block w-100 "></div>
                             </div>
                             <div class="carousel-item ">
+                                <div class="infoTextL2">
+                                    <h2 class="animate__animated animate__fadeInLeft">客製化<br>印花設計</h2>
+                                    <hr>
+                                    <p id="artist" class="animate__animated animate__flipInX animate__delay-1s">小小藝術家</p>
+                                </div>
                                 <svg width="0" height="0" aria-hidden="true">
                                     <defs>
                                         <mask id="clip3" maskUnits="userSpaceOnUse" x="0" y="0" width="1600" height="1080">
@@ -191,13 +206,12 @@
                                                 <circle class="anim-circle" cx="1116.84" cy="766.56" r="209.01" />
                                                 <circle class="anim-circle" cx="1038.7" cy="248.64" r="248.64" />
                                                 <circle class="anim-circle" cx="669.9" cy="200.24" r="168.93" />
-                                                <path class="anim-path" d="M1600,375.74c-40.39-51.17-102.96-84.01-173.21-84.01c-121.8,0-220.55,98.74-220.55,220.55s98.74,220.55,220.55,220.55
-    		c70.25,0,132.82-32.85,173.21-84.01V375.74z" />
+                                                <path class="anim-path" d="M1600,375.74c-40.39-51.17-102.96-84.01-173.21-84.01c-121.8,0-220.55,98.74-220.55,220.55s98.74,220.55,220.55,220.55 c70.25,0,132.82-32.85,173.21-84.01V375.74z" />
                                             </g>
                                         </mask>
                                     </defs>
                                 </svg>
-                                <div class="img-wrapper" id="mask3"><img src="./images/0003_1.jpg" class="d-block w-100 "></div>
+                                <div id="mask3"><img src="./images/0003_1.jpg" class="d-block w-100 "></div>
                             </div>
                         </div>
                         <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
@@ -217,7 +231,7 @@
         <div class="row text-center my-5">
             <div class="col-md-8 mx-auto">
                 <hr>
-                <h1>「花現生活，發現美一天。」</h1>
+                <p id="slogan">「花現生活，發現美一天。」</p>
                 <hr>
             </div>
         </div>
@@ -243,26 +257,39 @@
             </div>
         </div>
         <div class="row">
-            <div class="container-fluid brand">
-                <div class="d-flex my-4 col-md-10 mx-auto container">
+            <div class="container-fluid brand ">
+                <div class="d-flex col-md-10 my-5 mx-auto container">
                     <div class="col-8">
-                        <h4 class="px-3">品牌理念</h4>
-                        <p class="fs-5 px-3">花現小錄致力於將創意與藝術融入日常生活，透過精心設計的印花圖紋，讓每一件商品都成為獨特的藝術品。每一款印花圖案都源自對自然、文化和個性化風格的探索，旨在為消費者帶來不僅是商品，更是一種表達自我的方式。無論是日常穿著還是居家裝飾，希望每一件商品都能傳遞出個人獨特的態度，並讓每個細節都閃耀出無限的創造力與生活品味。</p>
+                        <h4 class="fs-2 px-3 to-animate opacity-0" data-animation="animate__fadeInUp">品牌理念</h4>
+                        <p class="fs-4 px-3 to-animate opacity-0" data-animation="animate__fadeInUp" data-delay="animate__delay-1s">花現小錄致力於將創意與藝術融入日常生活，透過精心設計的印花圖紋，讓每一件商品都成為獨特的藝術品。每一款印花圖案都源自對自然、文化和個性化風格的探索，旨在為消費者帶來不僅是商品，更是一種表達自我的方式。無論是日常穿著還是居家裝飾，希望每一件商品都能傳遞出個人獨特的態度，並讓每個細節都閃耀出無限的創造力與生活品味。</p>
                     </div>
-                    <div class="col-4"><img src="./images/brand.jpg" alt="" class="img-fluid ps-1"></div>
+                    <div class="col-4 to-animate opacity-0" data-animation="animate__zoomIn" data-delay="animate__delay-2s"><img src="./images/brand.jpg" alt="" class="img-fluid ps-1"></div>
                 </div>
             </div>
-            <div class="row my-5 mx-auto products">
-                <h3 class="text-center">產品展示</h3>
-                <div class="d-flex justify-content-around">
-                    <div class="product">
-                        <img src="./product_img/p001-1.jpg" alt="" class="img-fluid">
-                        <button type="button" class="btn-add-to-cart text-center ">加入購物車</button>
+            <div class="row products ">
+                <div class="my-5">
+                    <h4 class="text-center">產品展示</h4>
+                    <hr class="my-5">
+                    <div>
+                        <div class="d-flex justify-content-around my-3">
+                            <div class="product">
+                                <img src="./product_img/p001-1.jpg" alt="" class="img-fluid">
+                                <button type="button" class="btn-add-to-cart text-center ">加入購物車</button>
+                            </div>
+                            <div class="product">
+                                <img src="./product_img/p002-1.jpg" alt="" class="img-fluid">
+                                <button type="button" class="btn-add-to-cart text-center ">加入購物車</button>
+                            </div>
+                            <div class="product">
+                                <img src="./product_img/p003-1.jpg" alt="" class="img-fluid">
+                                <button type="button" class="btn-add-to-cart text-center ">加入購物車</button>
+                            </div>
+                            <div class="product">
+                                <img src="./product_img/p004-1.jpg" alt="" class="img-fluid">
+                                <button type="button" class="btn-add-to-cart text-center ">加入購物車</button>
+                            </div>
+                        </div>
                     </div>
-
-                    <div><img src="./product_img/p002-1.jpg" alt=""></div>
-                    <div><img src="./product_img/p003-1.jpg" alt=""></div>
-                    <div><img src="./product_img/p004-1.jpg" alt=""></div>
                 </div>
             </div>
         </div>
@@ -272,31 +299,91 @@
             <div id="last-data" class="row text-center">
                 <div class="col-sm-3 ">
                     <h4>ABOUT US</h4>
-                    <p><a>品牌故事</a></p>
-                    <p><a>歷年成果</a></p>
+                    <a href="#">
+                        <p>品牌故事</p>
+                    </a>
+                    <a href="#">
+                        <p>歷年成果</p>
+                    </a>
                 </div>
                 <div class="col-sm-3">
                     <h4>顧客服務</h4>
-                    <p><a>退換貨政策</a></p>
-                    <p><a>隱私政策</a></p>
-                    <p><a>運送方式</a></p>
+                    <a href="#">
+                        <p>退換貨政策</p>
+                    </a>
+                    <a href="#">
+                        <p>隱私政策</p>
+                    </a>
+                    <a href="#">
+                        <p>運送方式</p>
+                    </a>
                 </div>
                 <div class="col-sm-3">
                     <h4>門市資訊</h4>
-                    <p><a><i class="far fa-phone-alt"></i> tel 04-25229835</a></p>
-                    <p><a><i class="fas fa-door-open"></i> 周一至周五 8:00-18:30</a></p>
-                    <p><a><i class="fas fa-map-marker-alt"></i>門市地址</a></p>
+                    <a href="#">
+                        <p><i class="fas fa-phone"></i> tel 04-25229835</p>
+                    </a>
+                    <a href="#">
+                        <p><i class="fas fa-door-open"></i> 周一至周五 8:00-18:30</p>
+                    </a>
+                    <a href="#">
+                        <p><i class="fas fa-map-marker-alt"></i>門市地址</p>
+                    </a>
+                    <div class="icon d-flex justify-content-evenly">
+                        <a href="#"><p><i class="fab fa-facebook-square"></i></p></a>
+                        <a href="#"><p><i class="fab fa-instagram"></i></p></a>
+                        <a href="#"><p><i class="fab fa-line"></i></p></a>
+                        <a href="#"><p><i class="fab fa-youtube"></i></p></a>
+                    </div>
                 </div>
                 <div class="col-sm-3">
                     <h4>訂閱電子報</h4>
                     <p><input type="email" size="20" placeholder="example@email.com"></p>
+                    <button type="submit" class="btn btn-success">送出</button>
                 </div>
             </div>
         </div>
     </footer>
     <script>
-        
+        document.addEventListener("DOMContentLoaded", () => {
+            const brand = document.querySelector(".brand");
+            if (!brand) return;
+
+            const inner = brand.querySelectorAll(".to-animate");
+
+            const observer = new IntersectionObserver(([entry]) => {
+                if (entry.isIntersecting && entry.intersectionRatio >= 1) {
+                    inner.forEach(el => {
+                        // 清掉之前可能殘留的 animate__* class
+                        el.classList.forEach(c => {
+                            if (c.startsWith("animate__")) el.classList.remove(c);
+                        });
+
+                        // 強制重排讓移除生效
+                        void el.offsetWidth;
+
+                        // 加回指定的動畫 + delay + base
+                        const anim = el.dataset.animation;
+                        const delay = el.dataset.delay;
+                        if (anim) el.classList.add(anim);
+                        if (delay) el.classList.add(delay);
+                        el.classList.add("animate__animated");
+
+                        // 顯示
+                        el.classList.remove("opacity-0");
+                        // 拿掉 .to-animate 的 opacity 覆蓋
+                        // 或用el.style.opacity = ""; 
+                    });
+                    observer.unobserve(brand);
+                }
+            }, {
+                threshold: [1.0]
+            });
+
+            observer.observe(brand);
+        });
     </script>
+
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
