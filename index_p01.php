31bfd2c87ee045d7173a51fec86f2f486007e840
MizuAsagi0719 <hibawei@gmai.com>
2025-08-04 15:34:54 +0800

    v1.1 更新

--- index_p01.php
+++ index_p01.php
@@ -15,7 +15,7 @@
 
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg fixed-top">
+        <nav class="navbar navbar-expand-lg fixed-top" id="navbar">
             <div class="container-fluid">
                 <a class="navbar-brand col-2" href="#"><img src="./images/logo2.png" alt="" class="img-fluid"></a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -39,7 +39,6 @@
                 </div>
             </div>
         </nav>
-        <div class="demo"></div>
         <div class="container-fluid" id="content">
             <div class="row">
                 <div class="col-md-2 sidebar">
@@ -256,42 +255,52 @@
                 </button>
             </div>
         </div>
-        <div class="row">
-            <div class="container-fluid brand ">
-                <div class="d-flex col-md-10 my-5 mx-auto container">
-                    <div class="col-8">
-                        <h4 class="fs-2 px-3 to-animate opacity-0" data-animation="animate__fadeInUp">品牌理念</h4>
-                        <p class="fs-4 px-3 to-animate opacity-0" data-animation="animate__fadeInUp" data-delay="animate__delay-1s">花現小錄致力於將創意與藝術融入日常生活，透過精心設計的印花圖紋，讓每一件商品都成為獨特的藝術品。每一款印花圖案都源自對自然、文化和個性化風格的探索，旨在為消費者帶來不僅是商品，更是一種表達自我的方式。無論是日常穿著還是居家裝飾，希望每一件商品都能傳遞出個人獨特的態度，並讓每個細節都閃耀出無限的創造力與生活品味。</p>
-                    </div>
-                    <div class="col-4 to-animate opacity-0" data-animation="animate__zoomIn" data-delay="animate__delay-2s"><img src="./images/brand.jpg" alt="" class="img-fluid ps-1"></div>
+        <div class="container-fluid brand ">
+            <div class="row col-md-10 mx-auto ">
+                <div class="col-8 my-5">
+                    <h4 class="fs-2 px-3 to-animate opacity-0" data-animation="animate__fadeInUp">品牌理念</h4>
+                    <p class="fs-4 px-3 to-animate opacity-0" data-animation="animate__fadeInUp" data-delay="animate__delay-1s">花現小錄致力於將創意與藝術融入日常生活，透過精心設計的印花圖紋，讓每一件商品都成為獨特的藝術品。每一款印花圖案都源自對自然、文化和個性化風格的探索，旨在為消費者帶來不僅是商品，更是一種表達自我的方式。無論是日常穿著還是居家裝飾，希望每一件商品都能傳遞出個人獨特的態度，並讓每個細節都閃耀出無限的創造力與生活品味。</p>
+                </div>
+                <div class="col-4 to-animate opacity-0 my-5" data-animation="animate__zoomIn" data-delay="animate__delay-2s">
+                    <img src="./images/brand.jpg" alt="" class="img-fluid ps-1">
                 </div>
             </div>
-            <div class="row products ">
-                <div class="my-5">
-                    <h4 class="text-center">產品展示</h4>
-                    <hr class="my-5">
-                    <div>
-                        <div class="d-flex justify-content-around my-3">
-                            <div class="product">
-                                <img src="./product_img/p001-1.jpg" alt="" class="img-fluid">
-                                <button type="button" class="btn-add-to-cart text-center ">加入購物車</button>
-                            </div>
-                            <div class="product">
-                                <img src="./product_img/p002-1.jpg" alt="" class="img-fluid">
-                                <button type="button" class="btn-add-to-cart text-center ">加入購物車</button>
-                            </div>
-                            <div class="product">
-                                <img src="./product_img/p003-1.jpg" alt="" class="img-fluid">
-                                <button type="button" class="btn-add-to-cart text-center ">加入購物車</button>
-                            </div>
-                            <div class="product">
-                                <img src="./product_img/p004-1.jpg" alt="" class="img-fluid">
-                                <button type="button" class="btn-add-to-cart text-center ">加入購物車</button>
-                            </div>
-                        </div>
+        </div>
+        <div class="container-fluid products ">
+            <h4 class="text-center my-3">產品展示</h4>
+            <hr class="my-4">
+            <div class="row justify-content-around">
+                <div class="product col-md-2">
+                    <div class="p-body">
+                        <img src="./product_img/p001-1.jpg" alt="" class="img-fluid">
+                        <button type="button" class="btn-add-to-cart text-center ">加入購物車</button>
                     </div>
+                    <h5 class="card-title my-3">春日派對印花抱枕</h5>
                 </div>
+                <div class="product col-md-2">
+                    <div class="p-body">
+                        <img src="./product_img/p002-1.jpg" alt="" class="img-fluid">
+                        <button type="button" class="btn-add-to-cart text-center ">加入購物車</button>
+                    </div>
+                    <h5 class="card-title my-3">【客製】璇木童趣藍天包</h5>
+                </div>
+                <div class="product col-md-2">
+                    <div class="p-body">
+                        <img src="./product_img/p003-1.jpg" alt="" class="img-fluid">
+                        <button type="button" class="btn-add-to-cart text-center ">加入購物車</button>
+                    </div>
+                    <h5 class="card-title my-3">帆布包訂做</h5>
+                </div>
+                <div class="product col-md-2">
+                    <div class="p-body">
+                        <img src="./product_img/p004-1.jpg" alt="" class="img-fluid">
+                        <button type="button" class="btn-add-to-cart text-center ">加入購物車</button>
+                    </div>
+                    <h5 class="card-title my-3">度假連身裙</h5>
+                </div>
+
             </div>
+
         </div>
     </section>
     <footer>
@@ -330,10 +339,18 @@
                         <p><i class="fas fa-map-marker-alt"></i>門市地址</p>
                     </a>
                     <div class="icon d-flex justify-content-evenly">
-                        <a href="#"><p><i class="fab fa-facebook-square"></i></p></a>
-                        <a href="#"><p><i class="fab fa-instagram"></i></p></a>
-                        <a href="#"><p><i class="fab fa-line"></i></p></a>
-                        <a href="#"><p><i class="fab fa-youtube"></i></p></a>
+                        <a href="#">
+                            <p><i class="fab fa-facebook-square"></i></p>
+                        </a>
+                        <a href="#">
+                            <p><i class="fab fa-instagram"></i></p>
+                        </a>
+                        <a href="#">
+                            <p><i class="fab fa-line"></i></p>
+                        </a>
+                        <a href="#">
+                            <p><i class="fab fa-youtube"></i></p>
+                        </a>
                     </div>
                 </div>
                 <div class="col-sm-3">
@@ -384,6 +401,7 @@
         });
     </script>
 
+    <script src="./css/script.js"></script>
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
